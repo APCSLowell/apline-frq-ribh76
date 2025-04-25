d4860884b8be1d841348e390a1289f1fa150a22a
ribh76 <ribhat@s.sfusd.edu>
2025-04-24 19:48:28 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -4,12 +4,11 @@ public class APLine
   private int a; private int b; private int c; private double slope; 
   public void setSlope( int a, int b ) {
     slope  = (double) a / (double) b ; }
-  public double getslope { return slope; } 
-  public APLine ( int a, int b, int c,) { 
-                 a = a; b = b; c = c; }
-  public boolean isOnLine(x,y){ 
-    if ( 9a*y + b*y + + c)==0) return true; 
-    else return false; 
+  public double getslope() { return slope; } 
+  public APLine ( int a, int b, int c) { 
+                 this.a; this.b; this.c; }
+  public boolean isOnLine(int x, int y){ 
+    return ( (a*y + b*y + c)==0);
   } 
   
 }
